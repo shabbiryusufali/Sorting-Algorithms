c5a1425a4c32d86cb1238b1b65dc0a177b2acac1
shabbiryusufali <shabyusufali@gmail.com>
2021-04-19 02:01:52 -0700

    all

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -8,7 +8,7 @@ array and on an integer array
 #include "selectionsort.hpp"
 
 
-//Declare function to sort int array
+//A function to sort int array
 void selectionsort_int_array(int inputArray[], int size){
     for(int i = 0; i < size; i++){
         for(int j = i; j < size; j++){
@@ -21,7 +21,7 @@ void selectionsort_int_array(int inputArray[], int size){
     }
 }
 
-//Declare function to sort char* array
+//A function to sort char* array
 void selectionsort_string_array(char* inputArray[], int size){
     for(int i = 0; i < size; i++){
         for(int j = i; j < size; j++){
