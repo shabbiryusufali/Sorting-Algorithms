e757145d0bb4b819e64dc65edef9d87dba881ed5
shabbiryusufali <shabyusufali@gmail.com>
2021-04-19 01:06:36 -0700

    quicksort

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,1 +1,11 @@
+#include "quicksort.hpp"
 
+
+
+void quicksort_int_array(int* inputArray, int size){
+    
+}
+
+void quicksort_char_array(char* inputArray, int size){
+
+}
---
+++ quicksort.hpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+
+void quicksort_int_array(int* inputArray, int size);
+
+void quicksort_char_array(char* inputArray, int size);
